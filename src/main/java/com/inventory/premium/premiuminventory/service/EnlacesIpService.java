be78323c206ac0cc20f5c07358374993c60cd98e
kochoa-dot <70181138+kochoa-dot@users.noreply.github.com>
2023-08-20 23:46:22 -0600

    creating the contracts on EnlacesIp Repository

--- src/main/java/com/inventory/premium/premiuminventory/service/EnlacesIpService.java
+++ src/main/java/com/inventory/premium/premiuminventory/service/EnlacesIpService.java
@@ -1,4 +1,20 @@
 package com.inventory.premium.premiuminventory.service;
 
+import com.inventory.premium.premiuminventory.model.EnlacesIp;
+
+import java.util.List;
+
 public interface EnlacesIpService {
+
+    String saveEnlace(EnlacesIp enlacesIp);
+
+    public List<EnlacesIp> getAllEnlaces();
+
+    EnlacesIp getEnlaceById(Integer enlaceIpId);
+
+    EnlacesIp updateEnlace(EnlacesIp newEnlace, Integer enlaceIpId);
+
+    String deleteEnlace(Integer enlaceIpId);
+
+    EnlacesIp findByEnlace(String direccionIp);
 }
