6261b95eb6bfe1cd1ea2cca695b160d9a64b0fa4
kochoa-dot <70181138+kochoa-dot@users.noreply.github.com>
2023-08-23 12:34:43 -0600

    creating the model for Servicios

--- src/main/java/com/inventory/premium/premiuminventory/model/Servicios.java
+++ src/main/java/com/inventory/premium/premiuminventory/model/Servicios.java
@@ -1,4 +1,35 @@
 package com.inventory.premium.premiuminventory.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "Servicios")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Servicios {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SERVICIOID")
+    private int servicioId;
+
+    @Column(name = "NOMBRESERVICIO")
+    private String nombreServicio;
+
+    @ManyToOne
+    @JoinColumn(name = "ENLACEIPID")
+    private EnlacesIp enlacesIp;
+
+    @ManyToOne
+    @JoinColumn(name = "PROVEEDORID")
+    private Proveedores proveedores;
+
+    public Servicios(String nombreServicio, EnlacesIp enlacesIp, Proveedores proveedores) {
+        this.nombreServicio = nombreServicio;
+        this.enlacesIp = enlacesIp;
+        this.proveedores = proveedores;
+    }
 }
