1da297334ed045956ec08472169505766e8d7884
kochoa-dot <70181138+kochoa-dot@users.noreply.github.com>
2023-08-25 00:39:23 -0600

    Creating the model for usuarios

---
+++ src/main/java/com/inventory/premium/premiuminventory/security/model/Usuarios.java
@@ -0,0 +1,50 @@
+package com.inventory.premium.premiuminventory.security.model;
+
+import com.inventory.premium.premiuminventory.model.Estados;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.io.Serializable;
+
+@Entity
+@Table(schema = "dbo", name = "Usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuarios implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USUARIOID")
+    private int usuarioId;
+
+    @NotNull
+    @Column(unique = true, name = "NOMBREUSUARIO")
+    private String usuario;
+
+    @NotNull
+    @Column(name = "Correo")
+    private String correo;
+
+    @NotNull
+    @Column(name = "Contrasena")
+    private String contrasena;
+
+    @ManyToOne
+    @JoinColumn(name = "ROLID")
+    private Roles roles;
+
+    @ManyToOne
+    @JoinColumn(name = "ESTADOID")
+    private Estados estados;
+
+    public Usuarios(String usuario, String correo, String contrasena, Roles roles, Estados estados) {
+        this.usuario = usuario;
+        this.correo = correo;
+        this.contrasena = contrasena;
+        this.roles = roles;
+        this.estados = estados;
+    }
+}
