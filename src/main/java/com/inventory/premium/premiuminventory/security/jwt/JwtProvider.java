0fefa4b3b7e589a7da7f15da47491bca04a5480b
kochoa-dot <70181138+kochoa-dot@users.noreply.github.com>
2023-08-27 21:04:39 -0600

    creating some methods for jwt

--- src/main/java/com/inventory/premium/premiuminventory/security/jwt/JwtProvider.java
+++ src/main/java/com/inventory/premium/premiuminventory/security/jwt/JwtProvider.java
@@ -2,14 +2,19 @@ package com.inventory.premium.premiuminventory.security.jwt;
 
 
 import com.inventory.premium.premiuminventory.security.model.UsuarioPrincipal;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTParser;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
 
-
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,7 +32,7 @@ public class JwtProvider {
     /**
      *
      * @param authentication
-     * @return
+     * @return Un token que contiene el username, los roles, fecha de expiracion y una firma
      * */
 
     public String generateToken(Authentication authentication){
@@ -36,6 +41,50 @@ public class JwtProvider {
         return Jwts.builder()
                 .setSubject(usuarioPrincipal.getUsername())
                 .claim("roles", roles)
-                .setIs
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + expiration))
+                .signWith(SignatureAlgorithm.HS512, secret.getBytes())
+                .compact();
     }
+
+    /**
+     *
+     * @param token
+     * @return el username en el token
+     * */
+
+    public String getNombreUsuarioFromToken(String token){
+        return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJwt(token).getBody().getSubject();
+    }
+
+    /**
+     *
+     * @param token
+     * @return true si el token es valido o false si el token es invalido
+     */
+
+    public boolean validateToken(String token){
+        try {
+            Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJwt(token);
+            return true;
+        } catch (MalformedJwtException e){
+            logger.error("Token mal formado");
+        } catch (UnsupportedJwtException e){
+            logger.error("Token no soportado");
+        } catch (ExpiredJwtException e){
+            logger.error("Token expirado");
+        } catch (IllegalArgumentException e){
+            logger.error("Token vacio");
+        } catch (SignatureException e){
+            logger.error("Error en la firma");
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param jwtDto
+     * @return un nuevo token en caso de que el token que se le pase como argumento haya expirado
+     * @throws ParseException
+     */
 }
